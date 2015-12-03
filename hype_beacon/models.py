1a82b6e695663fd60acce6a64815b33b820dfe3a
brian <brian.lao@gmail.com>
2015-12-03 09:11:21 -0500

    fix typo

--- hype_beacon/models.py
+++ hype_beacon/models.py
@@ -11,5 +11,5 @@ class Beacon(models.Model):
     venue = models.ForeignKey('hype_venue.Venue')
 
     def __unicode__(self):
-        return self.uuid + '-' + str(self.major) + '-' + str(self.minor) + '-' + str(self.venue_)
+        return self.uuid + '-' + str(self.major) + '-' + str(self.minor) + '-' + str(self.venue)
 
