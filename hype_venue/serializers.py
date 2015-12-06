d25ed21b1908813499cd4bdcc7d3ab04014232b9
brian <brian.lao@gmail.com>
2015-12-06 12:19:08 -0500

    add fields to api

--- hype_venue/serializers.py
+++ hype_venue/serializers.py
@@ -8,4 +8,4 @@ class VenueSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Venue
-        fields = ('id', 'name', 'address', 'website', 'score')
+        fields = ('id', 'name', 'address', 'website', 'score', 'facebook_id', 'instagram_id', 'twitter_handle', 'hash_tags')
