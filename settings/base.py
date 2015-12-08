43d2407b9556141767f7cf77f570732578a58559
brian <brian.lao@gmail.com>
2015-12-07 23:56:16 -0500

    add cities light

--- settings/base.py
+++ settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'cities_light',
     'corsheaders',
     'hype_venue',
     'hype_beacon',
