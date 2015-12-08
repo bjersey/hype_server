06f4ccd310daeb7915a4d062c1be37fb20ca6592
brian <brian.lao@gmail.com>
2015-12-08 00:40:14 -0500

    add data migration

---
+++ hype_venue/migrations/0007_auto_20151208_0535.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def set_clarendon_region(apps, schema_editor):
+    Venue = apps.get_model("hype_venue", "Venue")
+    VenueRegion = apps.get_model("hype_venue", "VenueRegion")
+    venue_region = VenueRegion.objects.first()
+    for venue in Venue.objects.all():
+        venue.venue_region = venue_region
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hype_venue', '0006_auto_20151208_0532'),
+    ]
+
+    operations = [
+        migrations.RunPython(set_clarendon_region),
+    ]
