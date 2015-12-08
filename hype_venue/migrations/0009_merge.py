30e1dc20361497817cc1cc20f3eaa77960678c9d
brian <brian.lao@gmail.com>
2015-12-08 00:43:24 -0500

    make data migration

---
+++ hype_venue/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hype_venue', '0008_auto_20151208_0541'),
+        ('hype_venue', '0007_auto_20151208_0535'),
+    ]
+
+    operations = [
+    ]
