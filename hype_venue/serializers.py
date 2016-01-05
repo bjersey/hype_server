a8215055f122d0318f224b8a551e30cc3427619e
brian <brian.lao@gmail.com>
2016-01-05 00:34:34 -0500

    fix

--- hype_venue/serializers.py
+++ hype_venue/serializers.py
@@ -9,4 +9,4 @@ class VenueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Venue
         fields = ('id', 'name', 'address', 'website', 'score', 'facebook_id', 'instagram_id', 'twitter_handle',
-                  'hash_tags', 'instagram_followers')
+                  'hash_tags')
