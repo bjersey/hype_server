096cecb3961e9bd803c8e801729f3079b49cb11f
brian <brian.lao@gmail.com>
2016-01-17 15:11:44 -0500

    add search

--- hype_venue/admin.py
+++ hype_venue/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import Venue, VenueRegion, VenueInstagramStat
 
-admin.site.register(Venue)
+
+class VenueAdmin(admin.ModelAdmin):
+
+    search_fields = 'hash_tags'
+
+    class Meta:
+        model = Venue
+
+admin.site.register(Venue, VenueAdmin)
 admin.site.register(VenueRegion)
 admin.site.register(VenueInstagramStat)
