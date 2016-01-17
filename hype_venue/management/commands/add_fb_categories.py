14ef137016f3fb7d339cf66981941a03bcbc0072
brian <brian.lao@gmail.com>
2016-01-17 16:30:38 -0500

    debug statements

--- hype_venue/management/commands/add_fb_categories.py
+++ hype_venue/management/commands/add_fb_categories.py
@@ -17,7 +17,11 @@ class Command(BaseCommand):
 
             for venue in all_venues:
                 if venue.facebook_id:
-                    fb_object = graph.get_object(id=venue.facebook_id, fields='category_list')
+                    try:
+                        fb_object = graph.get_object(id=venue.facebook_id, fields='category_list')
+                    except Exception as e:
+                        print "inner exception"
+                        print e.message
 
                     for cat in fb_object['category_list']:
                         obj, _ = VenueCategory.objects.get_or_create(category=cat['name'])
@@ -25,4 +29,5 @@ class Command(BaseCommand):
                         venue.category.add(obj)
                         venue.save()
         except Exception as e:
-            print(e)
+            print "outer exception"
+            print e.message
