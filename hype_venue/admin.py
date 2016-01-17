7930d46cfab3197792d0bc5607fcd7816aed7e43
brian <brian.lao@gmail.com>
2016-01-17 15:31:02 -0500

    add category

--- hype_venue/admin.py
+++ hype_venue/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Venue, VenueRegion, VenueInstagramStat
+from .models import Venue, VenueRegion, VenueInstagramStat, VenueCategory
 
 
 class VenueAdmin(admin.ModelAdmin):
@@ -13,3 +13,4 @@ class VenueAdmin(admin.ModelAdmin):
 admin.site.register(Venue, VenueAdmin)
 admin.site.register(VenueRegion)
 admin.site.register(VenueInstagramStat)
+admin.site.register(VenueCategory)
