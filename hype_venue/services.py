2a43c548c8be8a0772220466c855836ac6964a29
brian <brian.lao@gmail.com>
2016-01-31 01:38:22 -0500

    use params from DB

--- hype_venue/services.py
+++ hype_venue/services.py
@@ -1,14 +1,22 @@
+from hype_venue.models import ScoreParameters
+
+
 def calc_venue_hype_score(venue, all_venue_fb_stat_by_venue_id, all_venue_twitter_stat_by_venue_id):
+
+    params = ScoreParameters.objects.get(name='FIT_score')
+
     fb_stat = all_venue_fb_stat_by_venue_id.get(venue.id)
     twitter_stat = all_venue_twitter_stat_by_venue_id.get(venue.id)
 
     if fb_stat:
-        fb_score = ((fb_stat.likes * 0.56) + (fb_stat.checkins * 0.44)) * 0.85
+        fb_score = ((fb_stat.likes * params.data['fb_likes_weight']) +
+                    (fb_stat.checkins * params.data['fb_checkins_weight'])) * params.data['fb_overall_weight']
     else:
         fb_score = 0
 
     if twitter_stat:
-        twitter_score = twitter_stat.followers_count * 0.15
+        twitter_score = (twitter_stat.followers_count * params.data['twitter_followers_weight']) * \
+                        params.data['twitter_overall_weight']
     else:
         twitter_score = 0
 
