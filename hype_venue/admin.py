f7d08355bd2c32a86a6ab390dc50dfa11fb3cf35
brian <brian.lao@gmail.com>
2016-01-31 01:51:34 -0500

    add action to calc scores

--- hype_venue/admin.py
+++ hype_venue/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.management import call_command
 
 from .models import Venue, VenueRegion, VenueInstagramStat, VenueCategory, VenueFacebookStat, VenueTwitterStat, ScoreParameters
 
@@ -12,10 +13,23 @@ class VenueAdmin(admin.ModelAdmin):
     class Meta:
         model = Venue
 
+
+class ScoreParametersAdmin(admin.ModelAdmin):
+
+    def update_scores(self, request, queryset):
+        call_command('calc_venue_scores')
+
+    actions = ['update_scores']
+
+    class Meta:
+        model = ScoreParameters
+
+
 admin.site.register(Venue, VenueAdmin)
+admin.site.register(ScoreParameters, ScoreParametersAdmin)
 admin.site.register(VenueRegion)
 admin.site.register(VenueInstagramStat)
 admin.site.register(VenueCategory)
 admin.site.register(VenueFacebookStat)
 admin.site.register(VenueTwitterStat)
-admin.site.register(ScoreParameters)
+add
