93e38c539a9fd1823fe141290b01c7b22dde69ad
brian <brian.lao@gmail.com>
2016-01-31 01:44:15 -0500

    cast to float for math

--- hype_venue/services.py
+++ hype_venue/services.py
@@ -9,14 +9,14 @@ def calc_venue_hype_score(venue, all_venue_fb_stat_by_venue_id, all_venue_twitte
     twitter_stat = all_venue_twitter_stat_by_venue_id.get(venue.id)
 
     if fb_stat:
-        fb_score = ((fb_stat.likes * params.data['fb_likes_weight']) +
-                    (fb_stat.checkins * params.data['fb_checkins_weight'])) * params.data['fb_overall_weight']
+        fb_score = ((fb_stat.likes * float(params.data['fb_likes_weight'])) +
+                    (fb_stat.checkins * float(params.data['fb_checkins_weight']))) * float(params.data['fb_overall_weight'])
     else:
         fb_score = 0
 
     if twitter_stat:
-        twitter_score = (twitter_stat.followers_count * params.data['twitter_followers_weight']) * \
-                        params.data['twitter_overall_weight']
+        twitter_score = (twitter_stat.followers_count * float(params.data['twitter_followers_weight'])) * \
+                        float(params.data['twitter_overall_weight'])
     else:
         twitter_score = 0
 
